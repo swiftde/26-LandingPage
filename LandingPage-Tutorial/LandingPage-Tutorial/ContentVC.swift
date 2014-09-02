cf121dfa1f0b97c13fdf2638de18a898110a416c
BenchR267 <herzog.benjamin@me.com>
2014-09-02 08:42:21 +0200

    inital commit Beta 6

---
+++ LandingPage-Tutorial/LandingPage-Tutorial/ContentVC.swift
@@ -0,0 +1,47 @@
+//
+//  ContentVC.swift
+//  LandingPage-Tutorial
+//
+//  Created by Benjamin Herzog on 02.09.14.
+//  Copyright (c) 2014 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ContentVC: UIViewController {
+    
+    var pageIndex: Int!
+    var imageString: String?
+    var titleString: String?
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if pageIndex != 4 {
+            startButton.hidden = true
+        }
+        titleLabel.text = titleString!
+        imageView.image = UIImage(named: imageString)
+    }
+    @IBAction func startButtonPressed(sender: AnyObject) {
+        NSUserDefaults.standardUserDefaults().setBool(true, forKey: "skipTutorial")
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as ViewController
+        (UIApplication.sharedApplication().delegate as AppDelegate).window?.rootViewController = vc
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ LandingPage-Tutorial/LandingPage-Tutorial/PageVC.swift
@@ -0,0 +1,93 @@
+//
+//  PageVC.swift
+//  LandingPage-Tutorial
+//
+//  Created by Benjamin Herzog on 02.09.14.
+//  Copyright (c) 2014 Benjamin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIViewController, UIPageViewControllerDataSource {
+
+    var pageViewController = UIPageViewController()
+    var pageImages = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blackColor()
+        
+        for i in 1...5 {
+            pageImages.append("page\(i).png")
+        }
+        pageViewController = storyboard.instantiateViewControllerWithIdentifier("PageViewController") as UIPageViewController
+        pageViewController.dataSource = self
+        
+        let viewController = [viewControllerAtIndex(0)!]
+        pageViewController.setViewControllers(viewController, direction: .Forward, animated: false, completion: nil)
+        
+        addChildViewController(pageViewController)
+        view.addSubview(pageViewController.view)
+        pageViewController.didMoveToParentViewController(self)
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController!, viewControllerBeforeViewController viewController: UIViewController!) -> UIViewController! {
+        let index = (viewController as ContentVC).pageIndex
+        if index == 0 {
+            return nil
+        }
+        return viewControllerAtIndex(index - 1)
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController!, viewControllerAfterViewController viewController: UIViewController!) -> UIViewController! {
+        let index = (viewController as ContentVC).pageIndex
+        if index + 1 == pageImages.count {
+            return nil
+        }
+        return viewControllerAtIndex(index + 1)
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController!) -> Int {
+        return 0
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController!) -> Int {
+        return pageImages.count
+    }
+    
+    func viewControllerAtIndex(index: Int) -> ContentVC? {
+        if pageImages.count == 0 || index >= pageImages.count {
+            return nil
+        }
+        
+        let contentVC = storyboard.instantiateViewControllerWithIdentifier("ContentVC") as ContentVC
+        contentVC.imageString = pageImages[index]
+        contentVC.titleString = pageImages[index]
+        contentVC.pageIndex = index
+        
+        return contentVC
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
